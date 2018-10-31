8d2c4cedb668112a26fb467bcf85f6501463db8b
papagalo <sjlewis919@gmail.com>
2018-10-31 14:53:05 -0400

    fixed seg fault. h_c, m_c, e_c wrong

--- csim.c
+++ csim.c
@@ -12,7 +12,7 @@
  *
  */
 
-struct Cache {
+typedef struct {
     int numSets;
     int numSetBits;
     int linesPerSet;
@@ -20,15 +20,15 @@ struct Cache {
     int blockSize;
     int numTagBits;
     unsigned long int ** tags;
-};
+}Cache;
 
 //Function Declarations
 int parseCommandLine(int argc, char **argv, int *cache_s, int *cache_E, int *cache_b, char **trace, int *vflag);
 uint64_t getBits(int start, int end, unsigned long bits);
 void errorMessage();
-void createCache(struct Cache *c, int numSetBits, int linesPerSet, int numBlockBits);
-int parseTraceFile(FILE * pf, char ** trace, struct Cache *cache, int * h_c, int * m_c, int * e_c);
-void insertAndAdjustLRU(struct Cache * cache, unsigned long tag, int setNum);
+Cache * createCache(int numSetBits, int linesPerSet, int numBlockBits);
+int parseTraceFile(FILE * pf, char ** trace, Cache *cache, int * h_c, int * m_c, int * e_c);
+void insertAndAdjustLRU(Cache * cache, unsigned long int tag, int setNum);
 
 //Global Variable(s)
 int verboseFlag = 0;
@@ -72,22 +72,37 @@ uint64_t getBits(int start, int end, unsigned long address) {
  * Params: cache pointer, s, E, and b
  *
  */
-void createCache(struct Cache *c, int numSetBits, int linesPerSet, int numBlockBits) {
+Cache * createCache(int numSetBits, int linesPerSet, int numBlockBits) {
     int i, j;
-    c->numSetBits = numSetBits;
-    c->numSets = (2 << numSetBits);
-    c->linesPerSet = linesPerSet;
-    c->numBlockBits = numBlockBits;
-    c->blockSize = (2 << numBlockBits);
-    c->numTagBits = 63 - numSetBits - numBlockBits;
-    c->tags = malloc(c->numSets * c->linesPerSet * sizeof(unsigned long int));
-    //initialize tags to -1
-    for (i = 0; i < c->numSets; i++) {
-        for(j = 0; j < c->linesPerSet; j++) {
-            c->tags[i][j] = -1;
+    //allocate memory for the cache structure
+    Cache * c_ptr = malloc(sizeof(* c_ptr));
+    if(c_ptr != NULL) {
+        c_ptr->numSetBits = numSetBits;
+        c_ptr->numSets = (2 << numSetBits);
+        c_ptr->linesPerSet = linesPerSet;
+        c_ptr->numBlockBits = numBlockBits;
+        c_ptr->blockSize = (2 << numBlockBits);
+        c_ptr->numTagBits = 63 - numSetBits - numBlockBits;
+        //allocate memory for the 2D tags array
+        c_ptr->tags = malloc(c_ptr->numSets * linesPerSet * sizeof(unsigned long int));
+        c_ptr->tags = malloc(sizeof(unsigned long int) * c_ptr->numSets);
+        for(i = 0; i < c_ptr->numSets; i++) {
+            c_ptr->tags[i] = malloc(sizeof(unsigned long int) * linesPerSet);
         }
+        //initialize tags to -1
+        for(i = 0; i < c_ptr->numSets; i++) {
+            for(j = 0; j < linesPerSet; j++) {
+                c_ptr->tags[i][j] = -1;
+            }
+        }
+        /*
+        for (i = 0; i < c_ptr->numSets; i++) {
+            for(j = 0; j < linesPerSet; j++) {
+              c_ptr->tags[i][j] = -1;
+            }
+        } */  
     }
-    
+    return c_ptr;    
 }
 
 /*
@@ -101,7 +116,7 @@ void createCache(struct Cache *c, int numSetBits, int linesPerSet, int numBlockB
  *  Params: cache pointer, tag number, set number
  *
  */
-void insertAndAdjustLRU(struct Cache * cache, unsigned long tag, int setNum) {
+void insertAndAdjustLRU(Cache * cache, unsigned long int tag, int setNum) {
     int i;
     //iterate through the lines in the set and move them all down 1
     for(i = cache->linesPerSet - 1; i > 1; i--) {
@@ -118,13 +133,13 @@ void insertAndAdjustLRU(struct Cache * cache, unsigned long tag, int setNum) {
  *  Return: -1 if error. 0 if not.
  *
  */
-int parseTraceFile(FILE * pf, char ** traceFile, struct Cache * cache, int * h_c, int * m_c, int * e_c) {
+int parseTraceFile(FILE * pf, char ** traceFile, Cache * cache, int * h_c, int * m_c, int * e_c) {
     int i;
     char buf[80];
     uint64_t address;
     char option;
     int size, setNum;
-    unsigned long tag;
+    unsigned long int tag;
     int setStartIndex = cache->numBlockBits + cache->numSetBits;
 
     pf = fopen(* traceFile, "r");
@@ -139,31 +154,35 @@ int parseTraceFile(FILE * pf, char ** traceFile, struct Cache * cache, int * h_c
         setNum = getBits(setStartIndex, cache->numBlockBits + 1, address);
         tag = getBits(0, cache->numTagBits, address);
         for(i = 0; i < cache->linesPerSet; i++) {
-           if(cache->tags[setNum][i] != 1 && tag == cache->tags[setNum][i]) {     //If it is valid and matches the tag
+           printf("first for loop\n");
+           if(cache->tags[setNum][i] != 1 && tag == cache->tags[setNum][i]) { //If it is valid and matches the tag
+               printf("1\n");
+               printf("%c\n", option);
                 if (option == 'M') {
-                   h_c += 2;
+                   *h_c = *h_c + 2;
                 }
-                else h_c++;
+                else *h_c = *h_c + 1;
+                printf("hit_count: %d\n", *h_c);
                 insertAndAdjustLRU(cache, tag, setNum);
                 break;
            }
         }
-        m_c++;
+        *m_c = *m_c + 1;
         for(i = 0; i < cache->linesPerSet; i++) {
             if(cache->tags[setNum][i] == -1) {
                 cache->tags[setNum][i] = tag;
                 if(option == 'M') {
-                    h_c++;
+                    *h_c = *h_c + 1;
                 }
                 insertAndAdjustLRU(cache, tag, setNum);
                 break;
             }
         }
-        e_c++;
+        *e_c = *e_c + 1;
         for(i = 0; i < cache->linesPerSet; i++) {
             if(cache->tags[setNum][i] == tag) {
                 if(option == 'M') {
-                    h_c++;
+                    *h_c = *h_c + 1;
                 }
                 insertAndAdjustLRU(cache, tag, setNum);
                 break;
@@ -232,8 +251,9 @@ int main(int argc, char **argv)
     int numSetBits = 0;
     int numLinesPerSet = 0;
     int blockOffsetBits = 0;
-    struct Cache cache;
-    struct Cache * cacheP = &cache;
+    Cache cache;
+    Cache * cacheP = &cache;
+    //struct Cache * cacheP = &cache;
     int hit_count = 0;
     int miss_count = 0;
     int eviction_count = 0;
@@ -241,7 +261,7 @@ int main(int argc, char **argv)
     char * traceFile = malloc(20);
     int vflag;
     parseCommandLine(argc, argv, &numSetBits, &numLinesPerSet, &blockOffsetBits, &traceFile, &vflag);
-    createCache(cacheP, numSetBits, numLinesPerSet, blockOffsetBits);
+    cacheP = createCache(numSetBits, numLinesPerSet, blockOffsetBits);
     parseTraceFile(&pf, &traceFile, cacheP, &hit_count, &miss_count, &eviction_count);
     printSummary(hit_count, miss_count, eviction_count);
     return 0;
